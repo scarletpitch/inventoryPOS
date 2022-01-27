44dadaf223a9f0af75c6bfc2dc54e7a5796df492
scarletpitch <nnpicha@gmail.com>
2022-01-27 10:29:05 +0700

    2022-01-27 fix bootstrap for sign in page

--- index.php
+++ index.php
@@ -24,15 +24,15 @@ if(isset($_POST['btn_login'])) {
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
+    <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -48,7 +48,7 @@ if(isset($_POST['btn_login'])) {
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>INVENTORY</b>POS</a>
+            <a href="index2.html"><b>INVENTORY</b>POS</a>
         </div>
         <!-- /.login-logo -->
         <div class="login-box-body">
@@ -69,7 +69,7 @@ if(isset($_POST['btn_login'])) {
                     </div>
                     <!-- /.col -->
                     <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="btn_login">Log In</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="btn_login">Sign in</button>
                     </div>
                     <!-- /.col -->
                 </div>
@@ -82,11 +82,11 @@ if(isset($_POST['btn_login'])) {
     <!-- /.login-box -->
 
     <!-- jQuery 3 -->
-    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap 3.3.7 -->
-    <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- iCheck -->
-    <script src="../../plugins/iCheck/icheck.min.js"></script>
+    <script src="plugins/iCheck/icheck.min.js"></script>
     <script>
         $(function() {
             $('input').iCheck({
